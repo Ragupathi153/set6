dae345305a73a58e9792aa78cfabdb1bddcde1af
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2018-02-19 15:58:53 +0530

    Create prgm56.c

---
+++ prgm56.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char ch[20];
+    int i;
+    scanf("%s",&ch);
+    if((ch[i]>'0'||ch[i]<'9')&&(ch[i]>='a'||ch[i]>='z')&&(ch[i]>='A'||ch[i]>='Z'))
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
+
