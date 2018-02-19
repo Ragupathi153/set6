017ade12b2e7f7fdbca6d662c9ff74cd15ad94c3
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2018-02-19 16:11:01 +0530

    Create prgm62.c

---
+++ prgm62.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char ch[40];
+    int i;
+    scanf("%s",&ch);
+    if(ch[i]== '0' || ch[i]=='1')
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
+
+
