3a596508f9dd2b00d10f182d48ee420533ec30ec
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2018-02-19 16:44:18 +0530

    Create prgm68.c

---
+++ prgm68.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d",&n);
+    m=n%7;
+    if(m==0)
+    {
+        printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+    return 0;
+}
+
