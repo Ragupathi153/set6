a9482a9a7e866da4dff2a30343ee9426e459605e
Ragupathi153 <32413458+Ragupathi153@users.noreply.github.com>
2018-02-21 11:28:24 +0000

    Create prgm61.c

---
+++ prgm61.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char a[10];
+    int i,k;
+    printf("Enter the string");
+    scanf("%s",&a);
+    printf("Enter the number of letters to be displayed");
+    scanf("%d",&n);
+    for(i=0;i<k;i++)
+    {
+        printf("%c",a[i]);
+    }
+
+    return 0;
+}
+
